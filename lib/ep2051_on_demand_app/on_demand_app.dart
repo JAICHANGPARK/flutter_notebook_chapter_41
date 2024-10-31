4631718d9b1db7593ea768edd395bb02288887ca
JAICHANGPARK <aristojeff@gmail.com>
2024-10-31 18:55:20 +0900

    Update on_demand_app.dart

---
+++ lib/ep2051_on_demand_app/on_demand_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class OnDemandApp extends StatelessWidget {
+  const OnDemandApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
