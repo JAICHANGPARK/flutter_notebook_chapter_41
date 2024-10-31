a6f194be25740329a014843154f052f61b501fe4
JAICHANGPARK <aristojeff@gmail.com>
2024-10-31 18:57:23 +0900

    Create on_demand_home_page.dart

---
+++ lib/ep2051_on_demand_app/views/on_demand_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class OnDemandHomePage extends StatefulWidget {
+  const OnDemandHomePage({super.key});
+
+  @override
+  State<OnDemandHomePage> createState() => _OnDemandHomePageState();
+}
+
+class _OnDemandHomePageState extends State<OnDemandHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
