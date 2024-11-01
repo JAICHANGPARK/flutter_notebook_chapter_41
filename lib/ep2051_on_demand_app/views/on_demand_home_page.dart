f13c5b0f1e7c4b5ddd2ac9799b16163b1d06f8a7
JAICHANGPARK <aristojeff@gmail.com>
2024-11-01 18:03:09 +0900

    Update on_demand_home_page.dart

--- lib/ep2051_on_demand_app/views/on_demand_home_page.dart
+++ lib/ep2051_on_demand_app/views/on_demand_home_page.dart
@@ -165,7 +165,7 @@ class _OnDemandHomePageState extends State<OnDemandHomePage> {
                   );
                 },
               ),
-            )
+            ),
           ],
         ),
       ),
