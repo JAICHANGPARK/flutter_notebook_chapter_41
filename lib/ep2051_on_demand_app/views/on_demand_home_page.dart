3c1b66a56d2a631133daf3ba3511fd1e5589805d
JAICHANGPARK <aristojeff@gmail.com>
2024-11-01 17:49:58 +0900

    Update on_demand_home_page.dart

--- lib/ep2051_on_demand_app/views/on_demand_home_page.dart
+++ lib/ep2051_on_demand_app/views/on_demand_home_page.dart
@@ -15,49 +15,60 @@ class _OnDemandHomePageState extends State<OnDemandHomePage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [],
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
       bottomNavigationBar: SizedBox(
         height: 80,
-
         child: BottomNavigationBar(
-          backgroundColor: Colors.white,
-
+            backgroundColor: Colors.white,
             selectedItemColor: Colors.deepOrange,
             type: BottomNavigationBarType.fixed,
             items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              HugeIcons.strokeRoundedHome01,
-            ),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              HugeIcons.strokeRoundedPlayList,
-            ),
-            label: "Jobs",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_2_outlined,
-            ),
-            label: "Workers",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.checklist,
-            ),
-            label: "Services",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.chat_outlined,
-            ),
-            label: "Message",
-          ),
-        ]),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  HugeIcons.strokeRoundedHome01,
+                ),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  HugeIcons.strokeRoundedPlayList,
+                ),
+                label: "Jobs",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person_2_outlined,
+                ),
+                label: "Workers",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.checklist,
+                ),
+                label: "Services",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.chat_outlined,
+                ),
+                label: "Message",
+              ),
+            ]),
       ),
     );
   }
