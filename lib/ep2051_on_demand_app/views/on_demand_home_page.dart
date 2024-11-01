9867d735a0be77be667ee9525ef866d1627a7121
JAICHANGPARK <aristojeff@gmail.com>
2024-11-01 17:59:57 +0900

    Update on_demand_home_page.dart

--- lib/ep2051_on_demand_app/views/on_demand_home_page.dart
+++ lib/ep2051_on_demand_app/views/on_demand_home_page.dart
@@ -155,6 +155,7 @@ class _OnDemandHomePageState extends State<OnDemandHomePage> {
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
                   return Container(
+                    width: 240,
                     decoration: BoxDecoration(
                       color: Colors.orange,
                       borderRadius: BorderRadius.circular(8),
