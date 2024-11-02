25d2d322d92cc33e71563f35915871b07954dc39
JAICHANGPARK <aristojeff@gmail.com>
2024-11-03 08:42:59 +0900

    Update on_demand_job_page.dart

--- lib/ep2051_on_demand_app/views/on_demand_job_page.dart
+++ lib/ep2051_on_demand_app/views/on_demand_job_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class OnDemandJobPage extends StatefulWidget {
   const OnDemandJobPage({super.key});
@@ -10,18 +11,67 @@ class OnDemandJobPage extends StatefulWidget {
 class _OnDemandJobPageState extends State<OnDemandJobPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("My jobs"),
-        Container(
-          height: 48,
-          decoration: BoxDecoration(
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("My jobs"),
+          Gap(6),
+          Container(
+            height: 48,
+            decoration: BoxDecoration(
               border: Border.all(
-            color: Colors.grey[400]!,
-          )),
-        )
-      ],
+                color: Colors.grey[400]!,
+              ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.deepOrange[100],
+                    ),
+                    child: Text(
+                      "Active",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
+          Gap(6),
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [],
+                      ),
+                      Divider(),
+                      Row(
+                        children: [],
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
