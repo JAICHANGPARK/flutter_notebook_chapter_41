3bafcc9f01cd991e789655f89315307c1abafc01
JAICHANGPARK <aristojeff@gmail.com>
2024-11-03 08:39:10 +0900

    Create on_demand_job_page.dart

---
+++ lib/ep2051_on_demand_app/views/on_demand_job_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class OnDemandJobPage extends StatefulWidget {
+  const OnDemandJobPage({super.key});
+
+  @override
+  State<OnDemandJobPage> createState() => _OnDemandJobPageState();
+}
+
+class _OnDemandJobPageState extends State<OnDemandJobPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [],
+    );
+  }
+}
