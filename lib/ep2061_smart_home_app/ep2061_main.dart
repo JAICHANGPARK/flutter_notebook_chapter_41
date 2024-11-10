7cd65f4c66822daaac1c02e86577a90d84627e1b
JAICHANGPARK <aristojeff@gmail.com>
2024-11-10 10:22:01 +0900

    Update ep2061_main.dart

--- lib/ep2061_smart_home_app/ep2061_main.dart
+++ lib/ep2061_smart_home_app/ep2061_main.dart
@@ -1,10 +1,3 @@
 import 'package:flutter/material.dart';
 
-class  extends StatelessWidget {
-  const ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
