55eca1de885ef07cc050d8226d57dbd8c3661755
JAICHANGPARK <aristojeff@gmail.com>
2024-11-10 10:34:22 +0900

    Update smart_home_main_page.dart

--- lib/ep2061_smart_home_app/views/smart_home_main_page.dart
+++ lib/ep2061_smart_home_app/views/smart_home_main_page.dart
@@ -17,6 +17,7 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
           children: [
             Positioned.fill(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
