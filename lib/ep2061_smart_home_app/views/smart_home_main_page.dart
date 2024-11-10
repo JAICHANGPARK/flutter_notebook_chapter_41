ca3d4edce045bd7bd815b1c2ae0dcf0c86793c7a
JAICHANGPARK <aristojeff@gmail.com>
2024-11-10 10:29:16 +0900

    Update smart_home_main_page.dart

--- lib/ep2061_smart_home_app/views/smart_home_main_page.dart
+++ lib/ep2061_smart_home_app/views/smart_home_main_page.dart
@@ -12,71 +12,76 @@ class _SmartHomeMainPageState extends State<SmartHomeMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 32,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: Container(
-                decoration: ShapeDecoration(
-                  shape: StadiumBorder(),
-                  color: Colors.grey[100],
-                ),
-                padding: EdgeInsets.all(6),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      decoration: ShapeDecoration(
-                        shape: StadiumBorder(),
-                        color: Colors.lightGreenAccent,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Column(),
+            ),
+            Positioned(
+              bottom: 32,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Container(
+                  decoration: ShapeDecoration(
+                    shape: StadiumBorder(),
+                    color: Colors.grey[100],
+                  ),
+                  padding: EdgeInsets.all(6),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.lightGreenAccent,
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.home_filled,
+                            ),
+                            Gap(12),
+                            Text("Home")
+                          ],
+                        ),
+                        padding: EdgeInsets.all(8),
                       ),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.home_filled,
-                          ),
-                          Gap(12),
-                          Text("Home")
-                        ],
+                      Gap(12),
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.all(8),
+                        child: Icon(Icons.grid_view),
                       ),
-                      padding: EdgeInsets.all(8),
-                    ),
-                    Gap(12),
-                    Container(
-                      decoration: ShapeDecoration(
-                        shape: StadiumBorder(),
-                        color: Colors.white,
+                      Gap(12),
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.all(8),
+                        child: Icon(Icons.grid_view),
                       ),
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.grid_view),
-                    ),
-                    Gap(12),
-                    Container(
-                      decoration: ShapeDecoration(
-                        shape: StadiumBorder(),
-                        color: Colors.white,
-                      ),
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.grid_view),
-                    ),
-                    Gap(12),
-                    Container(
-                      decoration: ShapeDecoration(
-                        shape: StadiumBorder(),
-                        color: Colors.white,
-                      ),
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.notifications_none),
-                    )
-                  ],
+                      Gap(12),
+                      Container(
+                        decoration: ShapeDecoration(
+                          shape: StadiumBorder(),
+                          color: Colors.white,
+                        ),
+                        padding: EdgeInsets.all(8),
+                        child: Icon(Icons.notifications_none),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
