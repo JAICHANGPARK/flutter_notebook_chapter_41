f0480a98842159b486a83e025003580ef0df7669
JAICHANGPARK <aristojeff@gmail.com>
2024-11-14 18:33:34 +0900

    Update ep2064_main.dart

--- lib/ep2064_travel_mobile_app/ep2064_main.dart
+++ lib/ep2064_travel_mobile_app/ep2064_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_41/ep2064_travel_mobile_app/travel_mobile_app.dart';
 
 
 void main() {
+  runApp(TravelMobileApp());
 
-  
 }
