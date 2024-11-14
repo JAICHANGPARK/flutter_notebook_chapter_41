4d58d549d3bddfda46c27ba17c25c2fe7cfb3fa4
JAICHANGPARK <aristojeff@gmail.com>
2024-11-14 18:35:38 +0900

    Update travel_home_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_home_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelHomePage extends StatefulWidget {
   const TravelHomePage({super.key});
 
@@ -11,6 +10,26 @@ class TravelHomePage extends StatefulWidget {
 class _TravelHomePageState extends State<TravelHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              children: [
+                Container(
+                  height: 240,
+                  color: Colors.grey[100],
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
