8291abf48a94f54f47e10438da1dc43535e8165d
JAICHANGPARK <aristojeff@gmail.com>
2024-11-14 18:32:14 +0900

    Create travel_mobile_app.dart

---
+++ lib/ep2064_travel_mobile_app/travel_mobile_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class TravelMobileApp extends StatelessWidget {
+  const TravelMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
