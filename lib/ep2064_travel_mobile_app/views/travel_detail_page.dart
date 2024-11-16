142ababf2b94ca44e1840215530435bafdad1c37
JAICHANGPARK <aristojeff@gmail.com>
2024-11-16 09:05:00 +0900

    Update travel_detail_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_detail_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelDetailPage extends StatefulWidget {
   const TravelDetailPage({super.key});
 
@@ -11,6 +10,16 @@ class TravelDetailPage extends StatefulWidget {
 class _TravelDetailPageState extends State<TravelDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Column(
+              children: [],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
