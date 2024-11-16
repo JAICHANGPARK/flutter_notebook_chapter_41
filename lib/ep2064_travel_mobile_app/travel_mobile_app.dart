4734f1060cbd243571518b9bd2f60acdc02a9457
JAICHANGPARK <aristojeff@gmail.com>
2024-11-16 09:02:27 +0900

    Update travel_mobile_app.dart

--- lib/ep2064_travel_mobile_app/travel_mobile_app.dart
+++ lib/ep2064_travel_mobile_app/travel_mobile_app.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_41/ep2064_travel_mobile_app/views/travel_home_page.dart';
-
+import 'package:go_router/go_router.dart';
 
 class TravelMobileApp extends StatelessWidget {
-  const TravelMobileApp({super.key});
+  TravelMobileApp({super.key});
+
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+          path: "/",
+          builder: (context, state) {
+            return TravelHomePage();
+          }),
+      GoRoute(
+          path: "/detail",
+          builder: (context, state) {
+            return TravelHomePage();
+          }),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
