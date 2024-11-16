2aa3335d4db2f59b29090674ce05c40234f8f134
JAICHANGPARK <aristojeff@gmail.com>
2024-11-16 09:05:57 +0900

    Update travel_detail_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_detail_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_detail_page.dart
@@ -11,15 +11,27 @@ class _TravelDetailPageState extends State<TravelDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Column(
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Column(
+                children: [],
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(
+              color: Colors.grey,
+            ))),
+            height: 80,
+            child: Row(
               children: [],
             ),
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
