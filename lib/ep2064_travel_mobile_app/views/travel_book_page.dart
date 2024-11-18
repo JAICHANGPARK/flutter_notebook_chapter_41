f341f16fb95c1be367f2b202d42f5fe3bc15f561
JAICHANGPARK <aristojeff@gmail.com>
2024-11-18 22:52:46 +0900

    Update travel_book_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_book_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_book_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class TravelBookPage extends StatefulWidget {
   const TravelBookPage({super.key});
@@ -11,38 +12,57 @@ class _TravelBookPageState extends State<TravelBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 200,
-          color: Colors.grey[100],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 24,
-                child: Icon(Icons.arrow_back),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Eiffel Tower"),
-                    Text("France"),
-                  ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 160,
+            padding: EdgeInsets.fromLTRB(20, 72, 20, 20),
+            color: Colors.grey[100],
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 24,
+                  child: Icon(Icons.arrow_back),
                 ),
-              ),
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 24,
-                child: Icon(Icons.favorite_outline),
-              ),
-            ],
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Eiffel Tower",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Gap(8),
+                      Text("France"),
+                    ],
+                  ),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 24,
+                  child: Icon(Icons.favorite_outline),
+                ),
+              ],
+            ),
           ),
-        )
-      ],
-    ));
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        
+      ),
+    );
   }
 }
