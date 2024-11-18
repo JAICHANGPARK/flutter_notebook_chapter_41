4e7d5c1898cae5b2fa44fa529223d0a35b68dc86
JAICHANGPARK <aristojeff@gmail.com>
2024-11-18 22:51:17 +0900

    Update travel_book_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_book_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_book_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelBookPage extends StatefulWidget {
   const TravelBookPage({super.key});
 
@@ -12,26 +11,38 @@ class _TravelBookPageState extends State<TravelBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 200,
-            color: Colors.grey[100],
-            child: Row(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.white,
-                  radius: 24,
-                  child: Icon(Icons.arrow_back),
-
-                )
-              ],
-            ),
-          )
-        ],
-      )
-    );
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 200,
+          color: Colors.grey[100],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 24,
+                child: Icon(Icons.arrow_back),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Eiffel Tower"),
+                    Text("France"),
+                  ],
+                ),
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 24,
+                child: Icon(Icons.favorite_outline),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
