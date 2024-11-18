4d50e11a77cd55e4592085fa0253e7b0acb25998
JAICHANGPARK <aristojeff@gmail.com>
2024-11-18 22:50:28 +0900

    Update travel_book_page.dart

--- lib/ep2064_travel_mobile_app/views/travel_book_page.dart
+++ lib/ep2064_travel_mobile_app/views/travel_book_page.dart
@@ -12,9 +12,25 @@ class _TravelBookPageState extends State<TravelBookPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [],
+        children: [
+          Container(
+            height: 200,
+            color: Colors.grey[100],
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 24,
+                  child: Icon(Icons.arrow_back),
+
+                )
+              ],
+            ),
+          )
+        ],
       )
     );
   }
