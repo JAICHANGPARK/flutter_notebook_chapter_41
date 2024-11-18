f747571c16d564a7e87b1400f35090f3ff57933d
JAICHANGPARK <aristojeff@gmail.com>
2024-11-18 22:46:03 +0900

    Update travel_book_page.dart

---
+++ lib/ep2064_travel_mobile_app/views/travel_book_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class TravelBookPage extends StatefulWidget {
+  const TravelBookPage({super.key});
+
+  @override
+  State<TravelBookPage> createState() => _TravelBookPageState();
+}
+
+class _TravelBookPageState extends State<TravelBookPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [],
+      )
+    );
+  }
+}
