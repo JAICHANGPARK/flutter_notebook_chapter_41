7baaba0e6944fcc6c706eadf071fac63dea3d593
JAICHANGPARK <aristojeff@gmail.com>
2024-11-19 18:33:21 +0900

    Update card_cafe_home_page.dart

--- lib/ep2070_card_cafe/views/card_cafe_home_page.dart
+++ lib/ep2070_card_cafe/views/card_cafe_home_page.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class CardCafeHomePage extends StatefulWidget {
+  const CardCafeHomePage({super.key});
+
+  @override
+  State<CardCafeHomePage> createState() => _CardCafeHomePageState();
+}
+
+class _CardCafeHomePageState extends State<CardCafeHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
