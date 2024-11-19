c639a1d447a72c43fa98d04b8ed5319dd0189989
JAICHANGPARK <aristojeff@gmail.com>
2024-11-19 18:32:22 +0900

    Update card_cafe_app.dart

---
+++ lib/ep2070_card_cafe/card_cafe_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CardCafeApp extends StatefulWidget {
+  const CardCafeApp({super.key});
+
+  @override
+  State<CardCafeApp> createState() => _CardCafeAppState();
+}
+
+class _CardCafeAppState extends State<CardCafeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
