193d2fc9736bcf3251eda90a361ddddaad1d9143
JAICHANGPARK <aristojeff@gmail.com>
2024-11-19 18:40:48 +0900

    Update card_cafe_app.dart

--- lib/ep2070_card_cafe/card_cafe_app.dart
+++ lib/ep2070_card_cafe/card_cafe_app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_41/ep2070_card_cafe/views/card_cafe_home_page.dart';
 
-
 class CardCafeApp extends StatefulWidget {
   const CardCafeApp({super.key});
 
@@ -13,6 +12,9 @@ class _CardCafeAppState extends State<CardCafeApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.teal,
+      ),
       home: CardCafeHomePage(),
     );
   }
