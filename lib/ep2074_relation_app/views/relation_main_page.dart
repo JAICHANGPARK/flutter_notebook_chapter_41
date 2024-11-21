6c61e23a2c89afa7e25087ffa79db1c6ab278990
Dreamwalker <aristojeff@gmail.com>
2024-11-21 16:44:22 +0900

    Update relation_main_page.dart

--- lib/ep2074_relation_app/views/relation_main_page.dart
+++ lib/ep2074_relation_app/views/relation_main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:gap/gap.dart';
 
 class RelationMainPage extends StatefulWidget {
   const RelationMainPage({super.key});
@@ -14,7 +14,34 @@ class _RelationMainPageState extends State<RelationMainPage> {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.apps,
+                    size: 32,
+                  ),
+                  Text("Relatons"),
+                  Spacer(),
 
+                  CircleAvatar(
+                    radius: 26,
+                  ),
+                  Gap(2),
+                  CircleAvatar(
+                    radius: 26,
+                  ),
+                  Gap(2),
+                  CircleAvatar(
+                    radius: 26,
+                  ),
+
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
