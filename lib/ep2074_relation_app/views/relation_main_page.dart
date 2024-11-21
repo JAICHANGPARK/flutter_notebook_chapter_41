586e56cf50110a65ee583f3fd56755284c5f36e6
Dreamwalker <aristojeff@gmail.com>
2024-11-21 16:41:48 +0900

    Create relation_main_page.dart

---
+++ lib/ep2074_relation_app/views/relation_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class RelationMainPage extends StatefulWidget {
+  const RelationMainPage({super.key});
+
+  @override
+  State<RelationMainPage> createState() => _RelationMainPageState();
+}
+
+class _RelationMainPageState extends State<RelationMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
