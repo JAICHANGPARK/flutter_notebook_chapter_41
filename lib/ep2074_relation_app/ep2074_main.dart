a20a8d6c9ed2b61c3a1be7e47248641f131e9cea
Dreamwalker <aristojeff@gmail.com>
2024-11-23 00:01:37 +0900

    Update ep2074_main.dart

--- lib/ep2074_relation_app/ep2074_main.dart
+++ lib/ep2074_relation_app/ep2074_main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_41/ep2074_relation_app/relation_app.dart';
 
 void main() {
-  runApp(RelationApp());
+  runApp(
+    RelationApp(),
+  );
 }
