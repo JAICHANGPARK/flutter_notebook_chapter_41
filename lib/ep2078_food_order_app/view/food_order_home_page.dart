d5c7ae648274bda2a9691b141c50fac6d05ce2dd
JAICHANGPARK <aristojeff@gmail.com>
2024-11-27 19:00:21 +0900

    Create food_order_home_page.dart

---
+++ lib/ep2078_food_order_app/view/food_order_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class FoodOrderHomePage extends StatefulWidget {
+  const FoodOrderHomePage({super.key});
+
+  @override
+  State<FoodOrderHomePage> createState() => _FoodOrderHomePageState();
+}
+
+class _FoodOrderHomePageState extends State<FoodOrderHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(child: child)
+        ],
+      ),
+    );
+  }
+}
