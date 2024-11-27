667f7f380856b1a82ef6dddbe39743ad9dea5723
JAICHANGPARK <aristojeff@gmail.com>
2024-11-27 19:00:23 +0900

    Update ep2078_main.dart

--- lib/ep2078_food_order_app/ep2078_main.dart
+++ lib/ep2078_food_order_app/ep2078_main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_41/ep2078_food_order_app/food_order_app.dart';
 
 void main() {
- runApp(FoodOrderApp());
+  runApp(FoodOrderApp());
 }
