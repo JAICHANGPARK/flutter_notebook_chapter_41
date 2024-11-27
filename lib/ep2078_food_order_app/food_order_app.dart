07657fba79a34a0176840d9a636fb217fe5b44d4
JAICHANGPARK <aristojeff@gmail.com>
2024-11-27 19:01:53 +0900

    Update food_order_app.dart

--- lib/ep2078_food_order_app/food_order_app.dart
+++ lib/ep2078_food_order_app/food_order_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'view/food_order_home_page.dart';
+
 
 class FoodOrderApp extends StatefulWidget {
   const FoodOrderApp({super.key});
@@ -11,6 +13,8 @@ class FoodOrderApp extends StatefulWidget {
 class _FoodOrderAppState extends State<FoodOrderApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FoodOrderHomePage(),
+    );
   }
 }
