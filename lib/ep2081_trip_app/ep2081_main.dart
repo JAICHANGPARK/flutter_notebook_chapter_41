bcb2d092f9b226b22ef1a621989589e0fc6f2d9e
JAICHANGPARK <aristojeff@gmail.com>
2024-11-30 08:35:51 +0900

    Update ep2081_main.dart

--- lib/ep2081_trip_app/ep2081_main.dart
+++ lib/ep2081_trip_app/ep2081_main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_41/ep2081_trip_app/trip_planner_app.dart';
 
 void main() {
-  runApp(TripPlannerApp());
+  runApp(
+    TripPlannerApp(),
+  );
 }
