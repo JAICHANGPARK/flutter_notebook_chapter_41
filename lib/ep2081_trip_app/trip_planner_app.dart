c6ad9f823639add295636188a509ac5031487869
JAICHANGPARK <aristojeff@gmail.com>
2024-11-30 08:32:55 +0900

    Create trip_planner_app.dart

---
+++ lib/ep2081_trip_app/trip_planner_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TripPlannerApp extends StatelessWidget {
+  const TripPlannerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
