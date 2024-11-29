2f5847491c1d12983b036148858afc2ef0a0e817
JAICHANGPARK <aristojeff@gmail.com>
2024-11-30 08:34:24 +0900

    Update trip_planner_app.dart

--- lib/ep2081_trip_app/trip_planner_app.dart
+++ lib/ep2081_trip_app/trip_planner_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'view/trip_home_page.dart';
+
 class TripPlannerApp extends StatelessWidget {
   const TripPlannerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: TripHomePage(),
     );
   }
 }
