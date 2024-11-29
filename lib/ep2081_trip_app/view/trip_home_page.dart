6743db53c4bfc75f42c4d75ddfc27f1160ab0146
JAICHANGPARK <aristojeff@gmail.com>
2024-11-30 08:33:58 +0900

    Update trip_home_page.dart

---
+++ lib/ep2081_trip_app/view/trip_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TripHomePage extends StatefulWidget {
+  const TripHomePage({super.key});
+
+  @override
+  State<TripHomePage> createState() => _TripHomePageState();
+}
+
+class _TripHomePageState extends State<TripHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
