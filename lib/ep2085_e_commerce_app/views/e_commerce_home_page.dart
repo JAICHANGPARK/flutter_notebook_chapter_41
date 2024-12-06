affdfc3779ab5206136d7192c7a74a6ff3f58bdd
JAICHANGPARK <aristojeff@gmail.com>
2024-12-06 17:16:18 +0900

    Update e_commerce_home_page.dart

--- lib/ep2085_e_commerce_app/views/e_commerce_home_page.dart
+++ lib/ep2085_e_commerce_app/views/e_commerce_home_page.dart
@@ -278,6 +278,7 @@ class _ECommerceHomePageState extends State<ECommerceHomePage> {
                                             decoration: BoxDecoration(
                                               color: Colors.black.withOpacity(.1),
                                             ),
+                                            padding: EdgeInsets.all(16),
                                             child: Column(
                                               crossAxisAlignment: CrossAxisAlignment.start,
                                               children: [
