a8269469a81afaa76727e75bd426d1cbcc6b96a7
JAICHANGPARK <aristojeff@gmail.com>
2024-12-06 17:23:42 +0900

    Update e_commerce_detail_page.dart

--- lib/ep2085_e_commerce_app/views/e_commerce_detail_page.dart
+++ lib/ep2085_e_commerce_app/views/e_commerce_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ECommerceDetailPage extends StatefulWidget {
   const ECommerceDetailPage({super.key});
 
@@ -14,8 +13,22 @@ class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
     return Scaffold(
       backgroundColor: Colors.orange[50],
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
+          Expanded(
+              child: Column(
+            children: [],
+          )),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16),
+                )
+              ),
+            ),
+          )
         ],
       ),
     );
