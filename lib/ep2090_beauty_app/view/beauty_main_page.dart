c0b37993fd9de47c0e7157e8ee4aeaddfd1ddfb2
JAICHANGPARK <aristojeff@gmail.com>
2024-12-09 17:19:00 +0900

    Create beauty_main_page.dart

---
+++ lib/ep2090_beauty_app/view/beauty_main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BeautyMainPage extends StatefulWidget {
+  const BeautyMainPage({super.key});
+
+  @override
+  State<BeautyMainPage> createState() => _BeautyMainPageState();
+}
+
+class _BeautyMainPageState extends State<BeautyMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          
+        ],
+      ),
+    );
+  }
+}
