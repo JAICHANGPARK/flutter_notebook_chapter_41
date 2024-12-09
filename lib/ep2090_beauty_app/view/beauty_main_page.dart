5903b385825db762d895fb94834e3a0d9c414790
JAICHANGPARK <aristojeff@gmail.com>
2024-12-09 17:22:23 +0900

    Update beauty_main_page.dart

--- lib/ep2090_beauty_app/view/beauty_main_page.dart
+++ lib/ep2090_beauty_app/view/beauty_main_page.dart
@@ -12,22 +12,32 @@ class _BeautyMainPageState extends State<BeautyMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              HugeIcons.strokeRoundedHome01,
-            ),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.explore_outlined,
-            ),
-            label: "Discover",
-          )
-        ],
+      bottomNavigationBar:
+      BottomAppBar(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Icon(
+                  HugeIcons.strokeRoundedHome01,
+                ),
+                Text("Home")
+              ],
+            )
+           ,
+            Column(
+              children: [
+                Icon(
+                  Icons.explore_outlined,
+                ),
+                Text("Discover")
+              ],
+            )
+
+          ],
+        ),
       ),
+   
     );
   }
 }
