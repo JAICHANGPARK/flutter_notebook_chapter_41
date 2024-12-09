f9ec3eaa228ccf954173610956238c381586a55d
JAICHANGPARK <aristojeff@gmail.com>
2024-12-09 17:28:40 +0900

    Update beauty_home_widget.dart

--- lib/ep2090_beauty_app/view/beauty_home_widget.dart
+++ lib/ep2090_beauty_app/view/beauty_home_widget.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class BeautyHomeWidget extends StatefulWidget {
+  const BeautyHomeWidget({super.key});
+
+  @override
+  State<BeautyHomeWidget> createState() => _BeautyHomeWidgetState();
+}
+
+class _BeautyHomeWidgetState extends State<BeautyHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
