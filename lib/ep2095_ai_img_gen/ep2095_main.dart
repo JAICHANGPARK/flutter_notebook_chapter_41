70ad2fc61b24c50c785c9f6cc820e344600c4350
JAICHANGPARK <aristojeff@gmail.com>
2024-12-14 07:35:00 +0900

    Update ep2095_main.dart

---
+++ lib/ep2095_ai_img_gen/ep2095_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AIImageGeneratorApp extends StatelessWidget {
+  const AIImageGeneratorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
