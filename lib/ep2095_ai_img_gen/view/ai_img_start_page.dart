9f7d9ae44c08b0bb629dbb13a085e5d371d5c38b
JAICHANGPARK <aristojeff@gmail.com>
2024-12-14 07:36:32 +0900

    Update ai_img_start_page.dart

--- lib/ep2095_ai_img_gen/view/ai_img_start_page.dart
+++ lib/ep2095_ai_img_gen/view/ai_img_start_page.dart
@@ -1,3 +1,17 @@
 import 'package:flutter/material.dart';
 
 
+class AiImgStartPage extends StatelessWidget {
+  const AiImgStartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
