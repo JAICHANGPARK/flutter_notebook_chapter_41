7a36d7cf4767161e2c390b575f7630ee582a177b
JAICHANGPARK <aristojeff@gmail.com>
2024-12-13 17:15:11 +0900

    Update beauty_detail_page.dart

--- lib/ep2090_beauty_app/view/beauty_detail_page.dart
+++ lib/ep2090_beauty_app/view/beauty_detail_page.dart
@@ -102,10 +102,14 @@ class _BeautyDetailPageState extends State<BeautyDetailPage> {
                     Gap(12),
                     Text(
                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+                    Gap(6),
                     Row(
                       children: [
                         Text(
                           "View More",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                         Gap(6),
                         Icon(
