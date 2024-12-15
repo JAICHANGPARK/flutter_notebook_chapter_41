68943d76dc8ab1eeb00bce536630dad6947756ec
JAICHANGPARK <aristojeff@gmail.com>
2024-12-15 16:05:47 +0900

    Update ai_img_home_page.dart

---
+++ lib/ep2095_ai_img_gen/view/ai_img_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiImgHomePage extends StatefulWidget {
+  const AiImgHomePage({super.key});
+
+  @override
+  State<AiImgHomePage> createState() => _AiImgHomePageState();
+}
+
+class _AiImgHomePageState extends State<AiImgHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
