d9ea9f519077f59953793e739119433866bdad4a
JAICHANGPARK <aristojeff@gmail.com>
2024-12-15 16:07:21 +0900

    Create ai_img_template_pagfe.dart

---
+++ lib/ep2095_ai_img_gen/view/ai_img_template_pagfe.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiImgTemplatePage extends StatefulWidget {
+  const AiImgTemplatePage({super.key});
+
+  @override
+  State<AiImgTemplatePage> createState() => _AiImgTemplatePageState();
+}
+
+class _AiImgTemplatePageState extends State<AiImgTemplatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
