3b1d033a6a8a39c4f688ba6b8b480bcbef0cc160
JAICHANGPARK <aristojeff@gmail.com>
2024-12-17 18:39:44 +0900

    Update ai_img_prompt_detail_page.dart

---
+++ lib/ep2095_ai_img_gen/view/ai_img_prompt_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiImgPromptDetailPage extends StatefulWidget {
+  const AiImgPromptDetailPage({super.key});
+
+  @override
+  State<AiImgPromptDetailPage> createState() => _AiImgPromptDetailPageState();
+}
+
+class _AiImgPromptDetailPageState extends State<AiImgPromptDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
