eee722b1dd1ed7161a9413426e9290045eb8c4ca
JAICHANGPARK <aristojeff@gmail.com>
2024-12-17 18:46:00 +0900

    Update ai_img_prompt_detail_page.dart

--- lib/ep2095_ai_img_gen/view/ai_img_prompt_detail_page.dart
+++ lib/ep2095_ai_img_gen/view/ai_img_prompt_detail_page.dart
@@ -16,8 +16,15 @@ class _AiImgPromptDetailPageState extends State<AiImgPromptDetailPage> {
           Positioned.fill(
             child: Image.network(
               "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d6a9ac6b-68f9-48b5-988f-593d9e8e8de4/original=true,quality=90/45231417.jpeg",
+              fit: BoxFit.cover,
             ),
-          )
+          ),
+          Positioned.fill(
+              child: SafeArea(
+            child: Column(
+              children: [],
+            ),
+          ))
         ],
       ),
     );
