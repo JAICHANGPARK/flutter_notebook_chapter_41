0930a0862bf07340b39a8ca1848b085e5b6d8185
JAICHANGPARK <aristojeff@gmail.com>
2024-12-18 23:23:08 +0900

    Update makeup_home_page.dart

---
+++ lib/ep2099_makeup_app/vierw/makeup_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MakeupHomePage extends StatefulWidget {
+  const MakeupHomePage({super.key});
+
+  @override
+  State<MakeupHomePage> createState() => _MakeupHomePageState();
+}
+
+class _MakeupHomePageState extends State<MakeupHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
