698920061ef407f48561a1cfeb1d1fec40c30840
JAICHANGPARK <aristojeff@gmail.com>
2024-12-18 23:21:18 +0900

    Update ep2099_main.dart

---
+++ lib/ep2099_makeup_app/ep2099_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  
+}
+
+class MakeupApp extends StatelessWidget {
+  const MakeupApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
